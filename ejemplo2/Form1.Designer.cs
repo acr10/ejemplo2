2d5b637787c94b87cca5aa94eb9643eaf03153c1
hp <hp@DESKTOP-IERU7BI>
2021-02-26 00:30:52 -0600

    Agregar archivos de proyecto.

---
+++ ejemplo2/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace ejemplo2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(3, -2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(436, 442);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(580, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(145, 147);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ ejemplo2/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace ejemplo2
+{
+    public partial class Form1 : Form
+    {
+        int x, y;
+        int i;
+        int i2;
+        int i3;
+        int rgb1;
+        int rgb2;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Ciruculo");
+            listBox1.Items.Add("Rectangulo");
+        }
+        private void Click()
+        {
+
+
+
+        }
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = panel1.CreateGraphics();
+            Pen lapiz = new Pen(Color.Black);
+          
+
+          
+            if (listBox1.SelectedIndex == 0)
+                {
+              
+                for (i2 = 10; i2 <= 200; i2++)
+                {
+                    for(i3=0; i3 <= 150; i3+=50)
+                    {
+
+                        SolidBrush sb = new SolidBrush(Color.FromArgb(i3, i2, i2));
+                        g.DrawEllipse(lapiz, x - 50, y - 50, i, i);
+                        g.FillEllipse(sb, x - 50, y - 50, i, i);
+
+                    }
+
+                }
+
+            }
+
+
+
+
+            if ( listBox1.SelectedIndex == 1)
+            {
+
+                for (i2 = 10; i2 <= 200; i2++)
+                {
+                    for (i3 = 0; i3 <= 150; i3 += 50)
+                    {
+                        SolidBrush sb = new SolidBrush(Color.FromArgb(i2, i3, i2));
+                        g.DrawRectangle(lapiz, x - 50, y - 50, i, i);
+                        g.FillRectangle(sb, x - 50, y - 50, i, i);
+
+                    }
+
+                }
+
+            }
+        }
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+           
+            Point punto = new Point(e.X, e.Y);
+            x = punto.X;
+            y = punto.Y;
+            panel1.Invalidate();
+
+            // me incrementa la variable i 
+            int count = i;
+            count+=10;
+            i = count;
+            
+
+        }
+    }
+}
